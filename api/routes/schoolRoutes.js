ae3cb8645f2f3d198b1b310792a7076feda202ae
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-11-23 23:32:06 -0500

    add DELETE /schools/delete route

--- api/routes/schoolRoutes.js
+++ api/routes/schoolRoutes.js
@@ -2,4 +2,6 @@ const schoolRoutes = require('express').Router(),
       schoolController = require('./../controllers/schoolController');
 
 schoolRoutes.post('/add', schoolController.newSchool);
+schoolRoutes.delete('/delete', schoolController.removeSchool);
+
 module.exports = schoolRoutes;
