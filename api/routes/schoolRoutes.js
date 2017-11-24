b8a1fcb49f5f0a06aa993357792e4f316bb0b53e
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-11-24 00:32:50 -0500

    add GET /schools/ route

--- api/routes/schoolRoutes.js
+++ api/routes/schoolRoutes.js
@@ -1,6 +1,7 @@
 const schoolRoutes = require('express').Router(),
       schoolController = require('./../controllers/schoolController');
 
+schoolRoutes.get('/', schoolController.getAll);      
 schoolRoutes.post('/add', schoolController.newSchool);
 schoolRoutes.delete('/delete', schoolController.removeSchool);
 
