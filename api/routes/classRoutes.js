80108768697f0dfd45b29e1268a5cd191825c303
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-11-25 17:07:22 -0500

    add  POST /classes  to routes

--- api/routes/classRoutes.js
+++ api/routes/classRoutes.js
@@ -2,5 +2,6 @@ const classRouter = require('express').Router(),
     classController = require('./../controllers/classController');
 
 classRouter.get('/', classController.getAll);
+classRouter.post('/', classController.addClass);
 
 module.exports = classRouter;
