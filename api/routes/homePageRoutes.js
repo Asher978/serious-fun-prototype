24b881bacd96a1e71ef5c383b995a0ea4efd6e25
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-11-27 01:53:59 -0500

    add POST /home_page Route

--- api/routes/homePageRoutes.js
+++ api/routes/homePageRoutes.js
@@ -2,5 +2,6 @@ const homePageRouter = require('express').Router(),
     homePageController = require('./../controllers/homePageController');
 
 homePageRouter.get('/',homePageController.retrieveData);
+homePageRouter.post('/', homePageController.setData);
 
 module.exports = homePageRouter;
