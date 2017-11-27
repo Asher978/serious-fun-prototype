9d80f103c5b2359bbc445b7b0a9ffa7fbeda5313
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-11-27 01:53:27 -0500

    add method setData to homePageController

--- api/controllers/homePageController.js
+++ api/controllers/homePageController.js
@@ -11,6 +11,24 @@ const homePageController = {
                 homePage
             });
         })
+    },
+    setData : (req ,res) => {
+        HomePage.find({}, (err, homePage) => {
+            if(homePage.length>0){
+                res.send({
+                    "status" : "Error",
+                    "message" : "Home Page was already set"
+                })
+            } else {
+                let body = _.pick(req.body, 
+                    ['title1','content1','title2','content2','title3','content3','bodyTitle1', 'bodyTitle2']),
+                    new_Home = new HomePage(body);
+
+                new_Home.save().then(() => {
+                    res.send(new_Home);
+                });
+            }
+        })
     }
 }
 
