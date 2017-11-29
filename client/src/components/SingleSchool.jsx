03d8a4f698dc6772e0b1f6927751b15bf5fe66a3
asher978 <ashershaheen@gmail.com>
2017-11-29 11:23:50 -0500

    added a new layout to show pictures and content for the schools

--- client/src/components/SingleSchool.jsx
+++ client/src/components/SingleSchool.jsx
@@ -9,17 +9,24 @@ class SingleSChool  extends Component {
         console.log('this button works');
     }
     render(){
-
+        const school = this.props.data;
       return (
-      <div>
-          <h1>{this.props.data.schoolName}</h1>
-          <p>
-              {this.props.data.st_Addr}<br/>
-              {this.props.data.city} {this.props.data.state} {this.props.data.zipcode}
-          </p>
-          <Link to={`/detailedSchool/${this.props.data._id}`}>See Classes</Link>
-          <Link to={`/editSchool/${this.props.data._id}`}>Edit School</Link>
-      </div>
+        <div class="col-sm-6 col-md-4">        
+          <div class="thumbnail">
+            <img src={school.picture_url} alt="..." />
+            <div class="caption">
+                <h3>{school.schoolName}</h3>
+                <p>
+                    {this.props.data.st_Addr}<br/>
+                    {this.props.data.city} {this.props.data.state} {this.props.data.zipcode}</p>
+                <p>
+                    <Link className='btn btn-primary main-color-bg' to={`/detailedSchool/${this.props.data._id}`}>See Classes</Link>&nbsp;
+                    <Link className='btn btn-primary main-color-bg' to={`/editSchool/${this.props.data._id}`}>Edit School</Link>
+                </p>
+            </div>
+        </div>
+        </div>
+      
       )
     }
 }
