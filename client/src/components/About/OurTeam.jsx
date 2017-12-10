4237e9a6c13391b010c3fbaa3adc6c8d34cc0fcd
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-09 23:30:11 -0500

    implement cms to OurTeam Component

--- client/src/components/About/OurTeam.jsx
+++ client/src/components/About/OurTeam.jsx
@@ -1,93 +1,123 @@
-import React from "react";
-// import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import axios from 'axios';
 
-const OurTeam = props => {
-  return (
-    <div className="ourTeam">
-    <div className="ourTeamContainer">
-    <div className="ourTeamTop">
+class OurTeam extends Component {
+  state={
+    pageTitle:'OurTeam',
+    content: null,
+    dataLoaded:false
+  }
 
-    <div className="ourTeamTitle">Our Team</div>
+  componentDidMount() {
+    axios.get(`/page/${this.state.pageTitle}`).then(res => {
+      let { pageContent } = res.data;
+      console.log(pageContent);
+      this.setState({
+        content: pageContent,
+        dataLoaded: true
+      });
+    });
+  }
+  
+  renderTeamPage = () => {
+    let { content } = this.state;
+    return (
+      <div className="ourTeam">
+      <div className="ourTeamContainer">
+      <div className="ourTeamTop">
+  
+      <div className="ourTeamTitle">{content.mainH}</div>
+  
+      <div className="ourTeamFounderInfo">
+  
+      <div className="ourTeamFounderPicContainer">
+      <div className="ourTeamFounderPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>{content.founderName}</p>
+      <p>{content.founderTitle}</p>
+      </div>
+      </div>
+      <div className="ourTeamFounderText">{content.founderBio}</div>
+      </div>
+      </div>
+  
+      <div className="ourTeamMid">
+      <div className="ourTeamTitle">{content.mainH2}</div>
+      <div className="ourTeamMemberInfo">
+  
+      <div className="ourTeamMemberPicContainer">
+      <div className="ourTeamMemberPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>Name</p>
+      <p>Title</p>
+      </div>
+      </div>
+      <div className="ourTeamMemberPicContainer">
+      <div className="ourTeamMemberPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>Name</p>
+      <p>Title</p>
+      </div>
+      </div>
+      <div className="ourTeamMemberPicContainer">
+      <div className="ourTeamMemberPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>Name</p>
+      <p>Title</p>
+      </div>
+      </div>
+      
+      </div>
+      </div>
+  
+      <div className="ourTeamBottom">
+      <div className="ourTeamTitle">{content.mainH3}</div>
+      <div className="ourTeamMemberInfo">
+  
+      <div className="ourTeamMemberPicContainer">
+      <div className="ourTeamMemberPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>Name</p>
+      <p>Title</p>
+      </div>
+      </div>
+      <div className="ourTeamMemberPicContainer">
+      <div className="ourTeamMemberPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>Name</p>
+      <p>Title</p>
+      </div>
+      </div>
+      <div className="ourTeamMemberPicContainer">
+      <div className="ourTeamMemberPic"></div>
+      <div className="ourTeamMemberPicCap">
+      <p>Name</p>
+      <p>Title</p>
+      </div>
+      </div>
+      
+      </div>
+      </div>
+  
+      <div className="ourTeamInfo">
+      <div className="ourTeamInfoText">{content.lastContent}</div>
+      <div className="ourTeamInfoBtn">LOCATIONS</div>
+      
+      </div>
+      </div>
+      </div>
+    );
+  }
 
-    <div className="ourTeamFounderInfo">
-
-    <div className="ourTeamFounderPicContainer">
-    <div className="ourTeamFounderPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Sylvia Ramsaywak</p>
-    <p>Founder</p>
-    </div>
-    </div>
-    <div className="ourTeamFounderText">Sylvia Ramsaywak is the founder alvia hammock irony selvage lyft vexillologist. Cold-pressed copper mug whatever, yuccie pok pok franzen pop-up hell of lumbersexual slow-carb brooklyn. Waistcoat vaporware shaman, meditation live-edge biodiesel whatever wayfarers cray PBR&B meh messenger bag microdosing forage. Health goth la croix chia shabby chic venmo bushwick. 8-bit paleo thundercats helvetica</div>
-    </div>
-    </div>
-
-    <div className="ourTeamMid">
-    <div className="ourTeamTitle">Serious Fun Executive Staff</div>
-    <div className="ourTeamMemberInfo">
-
-    <div className="ourTeamMemberPicContainer">
-    <div className="ourTeamMemberPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Name</p>
-    <p>Title</p>
-    </div>
-    </div>
-    <div className="ourTeamMemberPicContainer">
-    <div className="ourTeamMemberPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Name</p>
-    <p>Title</p>
-    </div>
-    </div>
-    <div className="ourTeamMemberPicContainer">
-    <div className="ourTeamMemberPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Name</p>
-    <p>Title</p>
-    </div>
-    </div>
-    
-    </div>
-    </div>
-
-    <div className="ourTeamBottom">
-    <div className="ourTeamTitle">Serious Fun Board Members</div>
-    <div className="ourTeamMemberInfo">
-
-    <div className="ourTeamMemberPicContainer">
-    <div className="ourTeamMemberPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Name</p>
-    <p>Title</p>
-    </div>
-    </div>
-    <div className="ourTeamMemberPicContainer">
-    <div className="ourTeamMemberPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Name</p>
-    <p>Title</p>
-    </div>
-    </div>
-    <div className="ourTeamMemberPicContainer">
-    <div className="ourTeamMemberPic"></div>
-    <div className="ourTeamMemberPicCap">
-    <p>Name</p>
-    <p>Title</p>
-    </div>
-    </div>
-    
-    </div>
-    </div>
-
-    <div className="ourTeamInfo">
-    <div className="ourTeamInfoText">To view Serious Fun Site Supervisors and Directors, vist your location page.</div>
-    <div className="ourTeamInfoBtn">LOCATIONS</div>
-    
-    </div>
-    </div>
-    </div>
-  );
+  render(){
+    let { dataLoaded } = this.state;
+    return (
+      <div>
+        {dataLoaded ? this.renderTeamPage() : <h1>Loading.....</h1> }
+      </div>
+    )
+  }
+  
 };
 
 export default OurTeam;
