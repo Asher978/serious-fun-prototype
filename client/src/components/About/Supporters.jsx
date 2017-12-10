bd7d7e1a1a0d1cdffd6952d7448f5beabbf2e752
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-09 23:58:12 -0500

    implement cms to Supporters Component

--- client/src/components/About/Supporters.jsx
+++ client/src/components/About/Supporters.jsx
@@ -1,55 +1,85 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-const Supporters = props => {
-  return (
-    <div className="supportersContainer">
-    
-    <div className="supportersTop">
-    <div className="supportTitle">Our Supporters</div>
-    <p className="supportTitleText">Serious Fun is grateful for our private, public, foundation and government supporters.</p>
-    </div>
+class Supporters extends Component{
+  state={
+    pageTitle:'Supporters',
+    content: null,
+    dataLoaded:false
+  }
 
-    <div className="supportersMid">
-    <div className="supportLogos"></div>
-    <div className="supportLogos"></div>
-    </div>
+  componentDidMount() {
+    axios.get(`/page/${this.state.pageTitle}`).then(res => {
+      let { pageContent } = res.data;
+      console.log(pageContent);
+      this.setState({
+        content: pageContent,
+        dataLoaded: true
+      });
+    });
+  }
 
-    <div className="supportersBottom">
-    <p className="supportBottomTitle">Individual Donors</p>
+  renderSupportersPage = ()=> {
+    let { content } = this.state;
+    return (
+      <div className="supportersContainer">
+      
+      <div className="supportersTop">
+      <div className="supportTitle">{content.mainH}</div>
+      <p className="supportTitleText">{content.subCont}</p>
+      </div>
 
-    <div className="supportBottomContainer">
+      <div className="supportersMid">
+      <div className="supportLogos"></div>
+      <div className="supportLogos"></div>
+      </div>
 
-    <hr className="supportBottomHR"></hr>
-    <div className="supportDonors">
-    <p className="supportBottomSubTitle">SERIOUS FUN BENEFACTOR!</p>
-    <p className="supportBottomTextTop">($101+)</p>
-    <p className="supportBottomTextBottom">First and last name</p>
-    </div>
-    <hr className="supportBottomHR"></hr>
-    <div className="supportDonors">
-    <p className="supportBottomSubTitle">SERIOUS FUN!</p>
-    <p className="supportBottomTextTop">($51-101)</p>
-    <p className="supportBottomTextBottom">First and last name</p>
-    </div>
-    <hr className="supportBottomHR"></hr>
-    <div className="supportDonors">
-    <p className="supportBottomSubTitle">SERIOUS!</p>
-    <p className="supportBottomTextTop">($26-50)</p>
-    <p className="supportBottomTextBottom">First and last name</p>
-    </div>
-    <hr className="supportBottomHR"></hr>
-    <div className="supportDonors">
-    <p className="supportBottomSubTitle">FUN!</p>
-    <p className="supportBottomTextTop">(up to $25)</p>
-    <p className="supportBottomTextBottom">First and last name</p>
-    </div>
-    </div>
+      <div className="supportersBottom">
+      <p className="supportBottomTitle">{content.mainH2}</p>
 
-    </div>
+      <div className="supportBottomContainer">
 
-    </div>
-  );
+      <hr className="supportBottomHR"></hr>
+      <div className="supportDonors">
+      <p className="supportBottomSubTitle">SERIOUS FUN BENEFACTOR!</p>
+      <p className="supportBottomTextTop">($101+)</p>
+      <p className="supportBottomTextBottom">First and last name</p>
+      </div>
+      <hr className="supportBottomHR"></hr>
+      <div className="supportDonors">
+      <p className="supportBottomSubTitle">SERIOUS FUN!</p>
+      <p className="supportBottomTextTop">($51-101)</p>
+      <p className="supportBottomTextBottom">First and last name</p>
+      </div>
+      <hr className="supportBottomHR"></hr>
+      <div className="supportDonors">
+      <p className="supportBottomSubTitle">SERIOUS!</p>
+      <p className="supportBottomTextTop">($26-50)</p>
+      <p className="supportBottomTextBottom">First and last name</p>
+      </div>
+      <hr className="supportBottomHR"></hr>
+      <div className="supportDonors">
+      <p className="supportBottomSubTitle">FUN!</p>
+      <p className="supportBottomTextTop">(up to $25)</p>
+      <p className="supportBottomTextBottom">First and last name</p>
+      </div>
+      </div>
+
+      </div>
+
+      </div>
+    );
+  }
+
+  render(){
+    let { dataLoaded } = this.state;
+    return (
+      <div>
+        {dataLoaded ? this.renderSupportersPage() : <h1>Loading.....</h1> }
+      </div>
+    )
+  }
 };
 
 export default Supporters;
