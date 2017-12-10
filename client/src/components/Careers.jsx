7a85e3b383c736cce9cf0ef1052f1f6c981b63b2
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-09 19:49:46 -0500

    implement cms to Careers Component

--- client/src/components/Careers.jsx
+++ client/src/components/Careers.jsx
@@ -1,39 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
+class Careers extends Component {
+  state={
+    pageTitle:'Careers',
+    content: null,
+    dataLoaded:false
+  }
 
-const Careers = props => {
-  return (
-    <div className="career">
-    <div className="careerPic"></div>
-    <div className="careerContainer">
-    <div className="careerInfoBox">
-    <div className="careerInfoTitle">Who We’re Looking For…</div>
-    <div className="careerInfoText">
-    <p>Serious Fun After School, Inc. is a non-profit organization dedicated to providing arts enrichment during out of school time at affordable rates so that our children have the opportunity to participate in the best that New York has to offer.</p>
-    <p>Our priority is providing our students with the highest quality content while overseeing the safety and security of our students. To provide the highest quality content, Serious Fun partners with professional artists who are dedicated to providing age-appropriate experiences for our students and class sizes are limited for more individualized attention. Daily enrichment includes Dance, Drama, Music, STEM, Science, and Art, as well as academic support, social engagement and active play, during out of school time.</p>
-    <p>Our passion is to provide the children in our care a chance to explore their creativity, move and stretch their bodies, and engage with their peers in a safe, caring and fun environment.</p>
-    </div>
-    </div>
+  componentDidMount() {
+    axios.get(`/page/${this.state.pageTitle}`).then(res => {
+      let { pageContent } = res.data;
+      this.setState({
+        content: pageContent,
+        dataLoaded: true
+      });
+    });
+  }
 
-    <div className="careerInfoBox">
-    <div className="careerInfoTitle currentJob">Current Job Openings</div>
-    <div className="careerInfoText">
+  renderCareersPage = () => {
+    let { content } = this.state;
+    return (
+      <div className="career">
+      <div className="careerPic"></div>
+      <div className="careerContainer">
+      <div className="careerInfoBox">
+      <div className="careerInfoTitle">{content.mainh}</div>
+      <div className="careerInfoText">
+      <p>{content.paragraph}</p>
+      <p>{content.paragraph2}</p>
+      <p>{content.paragraph3}</p>
+      </div>
+      </div>
 
-    <div className="careerInfoTextLocation">
-    <div className="careerInfoTextLocationTitle">ASTORIA, NY</div>
-    <Link to="" className="careerInfoTextLocationText">Drawing and Painting Specialist</Link>
-    <Link to="" className="careerInfoTextLocationText">Traditional Dance Specialist</Link>
-    
-    </div>
-    </div>
-    
-    </div>
-    
-    </div>
-    
+      <div className="careerInfoBox">
+      <div className="careerInfoTitle currentJob">{content.Heading2}</div>
+      <div className="careerInfoText">
 
-    </div>
-  );
+      <div className="careerInfoTextLocation">
+      <div className="careerInfoTextLocationTitle">ASTORIA, NY</div>
+      <Link to="" className="careerInfoTextLocationText">Drawing and Painting Specialist</Link>
+      <Link to="" className="careerInfoTextLocationText">Traditional Dance Specialist</Link>
+      
+      </div>
+      </div>
+      
+      </div>
+      
+      </div>
+      
+
+      </div>
+    )
+  }
+
+  render(){
+    let { dataLoaded } = this.state;
+    return (
+      <div>
+        {dataLoaded ? this.renderCareersPage() : <h1>Loading.....</h1> }
+      </div>
+    )
+  }
 };
 
 export default Careers;
