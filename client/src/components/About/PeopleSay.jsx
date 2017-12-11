9f777c63b058661cde1355dc751281e3c7ca7816
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-10 23:26:30 -0500

    validation to check if there are no Reviews on the db

--- client/src/components/About/PeopleSay.jsx
+++ client/src/components/About/PeopleSay.jsx
@@ -23,30 +23,22 @@ class PeopleSay extends Component{
           })
       }));
   }
-  renderPeopleSayPage = ()=> {
+
+  handleReviews = () =>{
     let { content, reviews } = this.state;
-    return (
-      <div className="peopleSayContainer">
-        <div className="peopleSayTitle">{content.mainH}</div>
+    return(
+      <div className="peopleSayMidContainer">
+      <div className="peopleSayTop">
       
-        <div className="peopleSayFeedBox">
-          <div className="peopleSayFeedBoxArrow LeftArrow"> &#8249; </div>
-          <div className="peopleSayFeedText">“It’s a fantastic after school program and offers so much for the price. It’s worth every penny… The teachers are caring, compassionate and creative. I know my daughter is in good hands – and I appreciate the active nature of the program as well.”</div>
-          <div className="peopleSayFeedBoxArrow RightArrow"> &#8250; </div>
+        <div className="peopleSayLeftSec">
+          <div className="peopleSaySubTitles">{content.subH}</div>
+            {reviews.map((review) => review.typeOfR == 'SeriousFun' ? <div key={review._id}><p className="peopleSayText">"{review.quote}"</p><p className="peopleSayTextCredit">-{review.reviewer}</p></div> : null)}
         </div>
 
-        <div className="peopleSayMidContainer">
-        <div className="peopleSayTop">
-        
-          <div className="peopleSayLeftSec">
-            <div className="peopleSaySubTitles">{content.subH}</div>
-              {reviews.map((review) => review.typeOfR == 'SeriousFun' ? <div key={review._id}><p className="peopleSayText">"{review.quote}"</p><p className="peopleSayTextCredit">-{review.reviewer}</p></div> : null)}
-          </div>
-
-        <div className="peopleSayRightSec">
-        <div className="peopleSayImage RightImage"></div>
-        </div>
-        </div>
+      <div className="peopleSayRightSec">
+      <div className="peopleSayImage RightImage"></div>
+      </div>
+      </div>
       <div className="peopleSayMid">
       <div className="peopleSaySubTitles">{content.subH2}</div>
         {reviews.map((review) => review.typeOfR == 'Classes' ? <div key={review._id}><p className="peopleSayText">"{review.quote}"</p><p className="peopleSayTextCredit">-{review.reviewer}</p></div> : null)}
@@ -70,7 +62,22 @@ class PeopleSay extends Component{
       </div>  
       
       </div>
+      </div>
+    )
+  }
+  renderPeopleSayPage = ()=> {
+    let { content, reviews } = this.state;
+    return (
+      <div className="peopleSayContainer">
+        <div className="peopleSayTitle">{content.mainH}</div>
+      
+        <div className="peopleSayFeedBox">
+          <div className="peopleSayFeedBoxArrow LeftArrow"> &#8249; </div>
+          <div className="peopleSayFeedText">“It’s a fantastic after school program and offers so much for the price. It’s worth every penny… The teachers are caring, compassionate and creative. I know my daughter is in good hands – and I appreciate the active nature of the program as well.”</div>
+          <div className="peopleSayFeedBoxArrow RightArrow"> &#8250; </div>
         </div>
+        {reviews.length == 0 ? <h3>No reviews posted</h3> : this.handleReviews()}
+       
       </div>
     );
   }
