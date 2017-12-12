725b947ba234c659ed8a62241e29548ab1780090
asher978 <ashershaheen@gmail.com>
2017-12-12 17:49:41 -0500

    heroku deploy

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/seriousDB',
+  database: process.env.MONGODB_URI ||'mongodb://localhost:27017/seriousDB',
   secret: 'sdfsfwrrew23423423fsdf'
 }
