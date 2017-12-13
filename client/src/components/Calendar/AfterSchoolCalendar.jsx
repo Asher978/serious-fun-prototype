2b08bc16e907e5d5b50efda84723745da028e536
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-12-13 15:35:30 -0500

    two maps showing up on same FullCalendar
    - Working on ability to change the map.

--- client/src/components/Calendar/AfterSchoolCalendar.jsx
+++ client/src/components/Calendar/AfterSchoolCalendar.jsx
@@ -1,48 +1,65 @@
-import React from "react";
-
 import FullCalendar from "./FullCalendar";
+import React, { Component } from "react";
 
-const AfterSchoolCalendar = props => {
-  return (
-    <div className="CalendarContainer afterSchoolCalendar">
-      <p className="CalendarMainTitle afterSchoolCalendarMainTitle">
-        Calendar - After School
-      </p>
+class AfterSchoolCalendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ""
+    };
+    this.afterSchoolCalendar = this.afterSchoolCalendar.bind(this);
+  }
+  componentDidMount() {}
 
-      <div className="CalendarTopInnerContainer afterSchoolCalendarTopInnerContainer">
-        <p className="CalendarSubDate afterSchoolCalendarSubDate">
-          <b>Current Program Dates:</b> September 11, 2017 - June 1, 2018
+  afterSchoolCalendar(e) {
+    this.setState({
+      value: e.target.value
+    });
+    // console.log(this.state.value, "Value");
+    // console.log(e.target.value, "Event");
+  }
+
+  render() {
+    console.log(this.state.value, "Value");
+    return (
+      <div className="CalendarContainer afterSchoolCalendar">
+        <p className="CalendarMainTitle afterSchoolCalendarMainTitle">
+          Calendar - After School
         </p>
 
-        <select
-          name="CalendarAfterSchool"
-          className="CalendarLocationDropMenu afterSchoolCalendarLocationDropMenu"
-        >
-          <option value="Select Location" selected>
-            Select Location
-          </option>
-          <option value="PS17Q">PS 17 Q</option>
-          <option value="PS85Q">PS 85 Q</option>
-          <option value="PSIS78QK1">PS/IS 78 Q (K-1)</option>
+        <div className="CalendarTopInnerContainer afterSchoolCalendarTopInnerContainer">
+          <p className="CalendarSubDate afterSchoolCalendarSubDate">
+            <b>Current Program Dates:</b> September 11, 2017 - June 1, 2018
+          </p>
 
-          <option value="PSIS78Q25">PS/IS 78 Q (2-5)</option>
-          <option value="PS150Q">PS 150 Q</option>
-          <option value="PS166Q">PS 166 Q</option>
+          <select
+            name="CalendarAfterSchool"
+            className="CalendarLocationDropMenu afterSchoolCalendarLocationDropMenu"
+            onChange={e => this.afterSchoolCalendar(e)}
+            value={this.state.value}
+          >
+            <option value="Select Location" defaultValue>
+              Select Location
+            </option>
+            <option value="PS17Q">PS 17 Q</option>
+            <option value="PS85Q">PS 85 Q</option>
+            <option value="PSIS78QK1">PS/IS 78 Q (K-1)</option>
 
-          <option value="PS33ChelseaPrep">PS 33 Chelsea Prep</option>
-        </select>
+            <option value="PSIS78Q25">PS/IS 78 Q (2-5)</option>
+            <option value="PS150Q">PS 150 Q</option>
+            <option value="PS166Q">PS 166 Q</option>
+
+            <option value="PS33ChelseaPrep">PS 33 Chelsea Prep</option>
+          </select>
+        </div>
+
+        <FullCalendar
+          SchoolCalendar={this.props.SchoolCalendar}
+          value={this.state.value}
+        />
       </div>
-
-      <FullCalendar />
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AfterSchoolCalendar;
-
-// <iframe
-// src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=l9dlc6er0krfdqto253o5384r4%40group.calendar.google.com&amp;color=%23A32929&amp;ctz=America%2FNew_York"
-// width="400"
-// height="415"
-// />
-
--- client/src/components/Calendar/FullCalendar.jsx
+++ client/src/components/Calendar/FullCalendar.jsx
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
-// import { Link } from "react-router-dom";
-// import $ from 'jquery';
+import React, { Component } from "react";
 
-// import * as $ from 'jquery';
-// window['jQuery'] = window['$'] = $;
 var $ = window.$;
 
 class FullCalendar extends Component {
@@ -13,15 +9,44 @@ class FullCalendar extends Component {
 
       eventSources: [
         {
-            googleCalendarId: "l9dlc6er0krfdqto253o5384r4@group.calendar.google.com"
+          googleCalendarId:
+            "l9dlc6er0krfdqto253o5384r4@group.calendar.google.com"
         },
         {
-            googleCalendarId: "55161l7ed74nkj2ifm41mcu8ao@group.calendar.google.com",
-            className: 'nice-event'
+          googleCalendarId:
+            "55161l7ed74nkj2ifm41mcu8ao@group.calendar.google.com",
+          className: "nice-event"
         }
-    ]
-
+      ]
     });
+  }
+
+
+
+
+  SchoolCalendar(){
+    let firstCalendar = "l9dlc6er0krfdqto253o5384r4@group.calendar.google.com";
+    let secCalendar = "55161l7ed74nkj2ifm41mcu8ao@group.calendar.google.com"; 
+    // $("#fullCalendar").fullCalendar({
+    //   googleCalendarApiKey: "AIzaSyDU_CakcARBmp2KzTSfDe7TO2Ta5jzaLS4",
+        
+    //   eventSources: [
+    //     {
+    //       googleCalendarId: firstCalendar,
+            
+    //     },
+    //     {
+    //       googleCalendarId:
+    //         secCalendar,
+    //       className: "nice-event"
+    //     }
+    //   ]
+    // });
+
+
+
+    console.log(this.props.value, "Value")
+
   }
 
   render() {
@@ -30,4 +55,3 @@ class FullCalendar extends Component {
 }
 
 export default FullCalendar;
-
