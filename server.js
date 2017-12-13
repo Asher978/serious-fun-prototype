a93323ceaef105c4b0e19787c5faa8a1bc769386
asher978 <ashershaheen@gmail.com>
2017-12-12 19:52:34 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ server.listen(port, () => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + '/public'));
 
 const userRoutes = require('./api/routes/userRoutes');
 app.use('/user', userRoutes);
@@ -70,6 +70,6 @@ const reviewRoutes = require('./api/routes/reviewRoutes');
 app.use('/api/reviews', reviewRoutes);
 
 app.get('*', (req, res) => {
-  const index = path.join(__dirname, 'build', 'index.html');
+  const index = path.join(__dirname, 'public', 'index.html');
   res.sendFile(index);
 })
