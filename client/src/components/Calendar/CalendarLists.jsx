ff70c3816980f9b1df7b0f5e941c3935d26a5a9a
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-12-17 21:02:34 -0500

    setting states - working

--- client/src/components/Calendar/CalendarLists.jsx
+++ client/src/components/Calendar/CalendarLists.jsx
@@ -3,23 +3,23 @@ import React, { Component } from "react";
 var CLIENT_ID =
   "981678339715-j3nk504vphs1v4tvcgjf61et7ied2bhr.apps.googleusercontent.com";
 var SCOPES = "https://www.googleapis.com/auth/calendar";
+
 class CalendarLists extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      calendarListIDs: []
+      calendarIds: []
     };
 
     this.authenticate = this.authenticate.bind(this);
     this.loadClient = this.loadClient.bind(this);
     this.execute = this.execute.bind(this);
+    this.SettingState = this.SettingState.bind(this);
   }
 
   componentDidMount() {
     window.gapi.load("client:auth2", function() {
       window.gapi.auth2.init({ client_id: CLIENT_ID, scope: SCOPES });
-      console.log("auth2 CLient ID");
     });
   }
 
@@ -38,7 +38,6 @@ class CalendarLists extends Component {
       );
   }
   loadClient() {
-    console.log("asdb");
     return window.gapi.client
       .load("https://content.googleapis.com/discovery/v1/apis/calendar/v3/rest")
       .then(
@@ -54,24 +53,28 @@ class CalendarLists extends Component {
   // Make sure the client is loaded and sign-in is complete before calling this method.
   execute() {
     let IDs = [];
-    window.gapi.client.calendar.calendarList.list({}).then(
+    this.SettingState(IDs);
+    return window.gapi.client.calendar.calendarList.list({}).then(
       function(response) {
         // Handle the results here (response.result has the parsed body).
-        response.result.items.map(function(ids) {
-          console.log(ids.id);
-          return this.setState({
-            calendarListIDs: ids.id
-          });
-        });
+        response.result.items.forEach(e => {
+          return IDs.push(e.id);
+        }, this);
       },
       function(error) {
         console.error("Execute error", error);
       }
     );
-    console.log(this.state.calendarListIDs, "checking");
+  }
+
+  SettingState(ids) {
+    this.setState({
+      calendarIds: ids
+    });
   }
 
   render() {
+    console.log("calendarIds", this.state.calendarIds);
     return (
       <div id="authorize-div" style={{ display: "" }}>
         <span>Authorize access to Google Calendar API</span>
